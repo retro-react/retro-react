a1f4d6403f7f04cb84e29926ffb32bcf404f0104
Irhad Fejzic <irhad.fejzic@gmail.com>
2023-02-28 21:05:22 +0100

    chore: run workflow

--- src/components/chip/Chip.tsx
+++ src/components/chip/Chip.tsx
@@ -6,7 +6,7 @@ export type Color = 'primary' | 'secondary' | 'success' | 'error' | 'warn';
 
 export interface ChipProps extends React.HTMLAttributes<HTMLDivElement> {
 	/**
-	 * The color of the Chip.
+	 * The color of the Chip component.
 	 *
 	 * @default 'primary'
 	 */
