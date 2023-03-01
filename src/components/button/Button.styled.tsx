a2728f95de73b7b7e32e7c14c0c4bfaffc16aa34
Irhad Fejzic <irhad.fejzic@gmail.com>
2023-03-01 21:34:51 +0100

    feat: adjust button pseudo-classes

--- src/components/button/Button.styled.tsx
+++ src/components/button/Button.styled.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { SHADE_6 } from '@src/constants/colors';
 import '../fonts.css';
 import type { Color, Variant } from './Button';
 import { getTheme } from './themes';
@@ -15,10 +16,21 @@ export const Button = styled.button<{ $color: Color; variant: Variant }>`
 	font-size: 0.875rem;
 	font-family: 'FrauncesLatin', sans-serif;
 	text-shadow: 2px 2px #000000;
+	cursor: pointer;
 	${(props) => getTheme(props.$color, props.variant)}
 
 	&:disabled {
 		cursor: not-allowed;
 		opacity: 0.6;
+	}
+
+	&:hover:not([disabled]) {
+		opacity: 0.8;
+		box-shadow: inset 0 8px 6px -6px ${SHADE_6};
+		filter: saturate(70%);
+	}
+
+	&:active:not([disabled]) {
+		filter: contrast(60%);
 	}
 `;
